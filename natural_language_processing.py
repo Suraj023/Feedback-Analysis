f02d487e82ae1b1dcbc2339bc9766a3ef9bec87e
Suraj <46607732+Suraj023@users.noreply.github.com>
2020-01-16 12:48:00 +0530

    Add files via upload

---
+++ natural_language_processing.py
@@ -0,0 +1,60 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3)
+
+# Cleaning the texts
+import re
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+corpus = []
+for i in range(0, 1000):
+    review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])
+    review = review.lower()
+    review = review.split()
+    ps = PorterStemmer()
+    review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+    review = ' '.join(review)
+    corpus.append(review)
+
+# Creating the Bag of Words model
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features = 1500)
+X = cv.fit_transform(corpus).toarray()
+y = dataset.iloc[:, 1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+#from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+# Fitting Naive Bayes to the Training set
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+def fun():
+    #name="The cashier had no care what so ever on what I had to say it still ended up being wayyy overpriced."
+    name=" "
+    if name!=" " :
+       ex=np.array([name])
+       example_count=cv.transform(ex)
+       p=classifier.predict(example_count.todense())
+       if p>[0]:
+          s="Positive"
+       else:
+          s="Negative"
+       return s
+    else:
+        return "No feedback"
+print(fun())
+
+
+
